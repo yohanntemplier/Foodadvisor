5a61a883acb29888263b7988afe9a474fd6bfccf
yohann <templier_yohann@yahoo.fr>
2019-11-28 11:14:08 +0100

    ajout du crud restaurant

---
+++ src/Controller/RestaurantController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Restaurant;
+use App\Repository\RestaurantRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/restaurant")
+ */
+class RestaurantController extends AbstractController
+{
+    /**
+     * @Route("/", name="restaurant_index", methods={"GET"})
+     */
+    public function index(RestaurantRepository $restaurantRepository): Response
+    {
+        return $this->render('restaurant/index.html.twig', [
+            'restaurants' => $restaurantRepository->findAll(),
+        ]);
+    }
+
+
+    /**
+     * @Route("/{id}", name="restaurant_show", methods={"GET"})
+     */
+    public function show(Restaurant $restaurant): Response
+    {
+        return $this->render('restaurant/show.html.twig', [
+            'restaurant' => $restaurant,
+        ]);
+    }
+}
