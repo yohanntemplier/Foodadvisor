8b7f21ee2d7e7a6f120c6cd3b17c1268f88f4686
yohann <templier_yohann@yahoo.fr>
2019-12-19 20:48:33 +0100

    ajout du multiupload

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,7 @@ require('../css/app.scss');
 /// app.js
 
 const $ = require('jquery');
+
 // this "modifies" the jquery module: adding behavior to it
 // the bootstrap module doesn't export/return anything
 require('bootstrap');
@@ -22,6 +23,30 @@ require('@fortawesome/fontawesome-free/js/all');
 
 $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
+});
+
+
+//suppression des elements
+document.querySelectorAll('[data-delete]').forEach(a => {
+    a.addEventListener('click', e => {
+        e.preventDefault()
+        fetch(a.getAttribute('href'), {
+            method: 'DELETE',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'_token': a.dataset.token})
+        }).then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    a.parentNode.parentNode.removeChild(a.parentNode)
+                } else {
+                    alert(data.error)
+                }
+            })
+            .catch(e => alert(e))
+    })
 });
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
---
+++ src/Migrations/Version20191219133958.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191219133958 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE picture (id INT AUTO_INCREMENT NOT NULL, filename VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('DROP TABLE caracteristic_restaurant');
+        $this->addSql('DROP TABLE paiement_restaurant');
+        $this->addSql('ALTER TABLE restaurant DROP pictures');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE caracteristic_restaurant (caracteristic_id INT NOT NULL, restaurant_id INT NOT NULL, INDEX IDX_119595FE81194CF4 (caracteristic_id), INDEX IDX_119595FEB1E7706E (restaurant_id), PRIMARY KEY(caracteristic_id, restaurant_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('CREATE TABLE paiement_restaurant (paiement_id INT NOT NULL, restaurant_id INT NOT NULL, INDEX IDX_7D5A7CCF2A4C4478 (paiement_id), INDEX IDX_7D5A7CCFB1E7706E (restaurant_id), PRIMARY KEY(paiement_id, restaurant_id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE caracteristic_restaurant ADD CONSTRAINT FK_119595FE81194CF4 FOREIGN KEY (caracteristic_id) REFERENCES caracteristic (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE caracteristic_restaurant ADD CONSTRAINT FK_119595FEB1E7706E FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE paiement_restaurant ADD CONSTRAINT FK_7D5A7CCF2A4C4478 FOREIGN KEY (paiement_id) REFERENCES paiement (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE paiement_restaurant ADD CONSTRAINT FK_7D5A7CCFB1E7706E FOREIGN KEY (restaurant_id) REFERENCES restaurant (id) ON DELETE CASCADE');
+        $this->addSql('DROP TABLE picture');
+        $this->addSql('ALTER TABLE restaurant ADD pictures VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_unicode_ci`');
+    }
+}
